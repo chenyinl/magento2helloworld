a8a26ba81ebc63e25d01a0f45679009ad50be0db
CHEN <chenyinl@yahoo.com>
2017-12-08 14:17:55 -0800

    First Commit

---
+++ HelloWorld/Block/Catalog/Product/HelloWorld.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @var \Amasty\HelloWorld\Helper\Data
+ */
+namespace Amasty\HelloWorld\Block\Catalog\Product;
+
+class HelloWorld extends \Magento\Framework\View\Element\Template
+{
+    public function __construct(
+    \Magento\Framework\View\Element\Template\Context $context,
+    array $data = []
+) {
+    parent::__construct($context, $data);
+    }
+}
---
+++ HelloWorld/Helper/Data.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @var \Amasty\HelloWorld\Helper\Data
+ */
+ /*
+protected $_helper;
+ 
+public function __construct(
+    \Magento\Framework\View\Element\Template\Context $context,
+    array $data = [],
+    \Amasty\HelloWorld\Helper\Data $helper
+) {
+    parent::__construct($context, $data);
+ 
+    $this->_helper = $helper;
+}
+*/
